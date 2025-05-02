0f489547dd9f8ff8c456af5c5b112a23dd578c4f
zoia_plus <0110pzp@gmail.com>
2025-05-02 18:05:49 +0200

    add index file

---
+++ src/generics/4.ts
@@ -0,0 +1,18 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
+
+function createOrUpdateUser(initialValues: Partial<User>) {
+  
+  console.log(initialValues);
+}
+
+createOrUpdateUser({ 
+  email: 'user@mail.com', 
+  password: 'password123' 
+});
+
+export {};
